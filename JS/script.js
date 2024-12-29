54410266b5dccb0ae6e7be3dcd80a478e6e600ba
TheH3rmit <95000621+TheH3rmit@users.noreply.github.com>
2024-12-29 04:04:55 +0100

    day night fixed

--- JS/script.js
+++ JS/script.js
@@ -4,16 +4,26 @@ document.addEventListener("DOMContentLoaded", () => {
     const themeToggle = document.getElementById('theme-toggle');
     const body = document.body;
 
-    // Hamburger menu toggle
-    hamburger.addEventListener("click", () => {
-        header.classList.toggle("expanded"); // Toggle the expanded class to show/hide the menu
-        const navigationList = document.querySelector("#navigation-list-mobile");
-        navigationList.classList.toggle("show"); // Toggle the visibility of the menu
-    });
+    // Check if theme preference is saved in localStorage
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        body.classList.toggle('bright-mode', savedTheme === 'bright');
+        themeToggle.innerHTML = savedTheme === 'bright' ? '<i class="fas fa-sun"></i>' : '<i class="fas fa-moon"></i>';
+    }
 
     // Day/Night view toggle
     themeToggle.addEventListener('click', () => {
         const isDarkMode = body.classList.toggle('bright-mode');
         themeToggle.innerHTML = isDarkMode ? '<i class="fas fa-sun"></i>' : '<i class="fas fa-moon"></i>';
+        
+        // Save theme preference in localStorage
+        localStorage.setItem('theme', isDarkMode ? 'bright' : 'dark');
+    });
+
+    // Hamburger menu toggle
+    hamburger.addEventListener("click", () => {
+        header.classList.toggle("expanded"); // Toggle the expanded class to show/hide the menu
+        const navigationList = document.querySelector("#navigation-list-mobile");
+        navigationList.classList.toggle("show"); // Toggle the visibility of the menu
     });
 });
